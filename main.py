20997a173627e282082db543f3f8fc7fde8a1e32
woojing <woojing.seok@gmail.com>
2021-03-09 09:06:25 +0900

    Added save and done function

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ TODO:
 [*] add task
 [*] change data structure
 [*] add index
-[ ] done task
+[*] done task
+[ ] save function
 [ ] remove task
 [ ] modify task
 
@@ -19,12 +20,44 @@ TODO:
 
 '''
 # data example
-# 0,"Go to zoo"
-# 1,"Take shower"
+# 0,todo,"Go to zoo"
+# 1,done,"Take shower"
 import os
+from typing import List
 
 
 SAVE_FILE = 'tasks.txt'
+
+def save_task(tasks: List[dict], mode: str, file_name: str = SAVE_FILE) -> None:
+    '''
+    {
+        'status': 'todo',
+        'name': 'task 1',
+    }
+    lines를 받아서 file_name에 lines를 저장한다.
+    mode는 'w', 'a'
+    '''
+    if mode == 'w':
+        str_tasks = [
+            f"{i},{task['status']},{task['name']}\n"
+            for i, task
+            in enumerate(tasks)
+        ]
+        with open(file_name, 'w') as f:
+            f.writelines(str_tasks)
+
+    if mode == 'a':
+        for task in tasks:
+            if os.path.exists(file_name):
+                save_file = open(file_name, 'r')
+                last_index = get_last_index(save_file.readlines())
+                next_index = last_index + 1
+                save_file.close()
+            else:
+                next_index = 0
+            save_file = open(file_name, 'a')
+            save_file.write(f"{next_index},{task['status']},{task['name']}\n")
+            save_file.close()
 
 def add_task(task: str, file_name: str = SAVE_FILE):
     if os.path.exists(file_name):
@@ -35,7 +68,7 @@ def add_task(task: str, file_name: str = SAVE_FILE):
     else:
         next_index = 0
     save_file = open(file_name, 'a')
-    save_file.write(f'{next_index},{task}\n')
+    save_file.write(f'{next_index},todo,{task}\n')
     save_file.close()
 
 def list_task(file_name: str = SAVE_FILE) -> list:
@@ -49,6 +82,18 @@ def get_last_index(lines: list) -> int:
     if not lines:
         return 0
     return max([int(line.split(',')[0]) for line in lines])
+
+def make_it_done(index: int, file_name: str = SAVE_FILE):
+    '''
+    status를 done으로 변경
+    '''
+    lines = list_task(file_name)
+    parsed_task = lines[index].split(',')
+    parsed_task[1] = 'done'
+    lines[index] = ','.join(parsed_task)
+    with open(file_name, 'w') as save_file:
+        for line in lines:
+            save_file.write(line + '\n')
 
 if __name__ == '__main__':
     add_task('hi')
--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 import os
 import unittest
 
-from main import add_task, list_task, get_last_index
+from main import (add_task, list_task, get_last_index, make_it_done, save_task)
 
 class TaskTest(unittest.TestCase):
     def setUp(self):
@@ -12,7 +12,7 @@ class TaskTest(unittest.TestCase):
             os.remove(self.file_name)
 
     def test_add_task(self):
-        expected_results = ['0,task1\n', '1,task2\n']
+        expected_results = ['0,todo,task1\n', '1,todo,task2\n']
         tasks = ['task1', 'task2']
         for task in tasks:
             add_task(task, self.file_name)
@@ -20,16 +20,13 @@ class TaskTest(unittest.TestCase):
         self.assertEqual(expected_results, test_file.readlines())
 
     def test_list_task(self):
-        expected_tasks = ['0,task one', '1,task two', '2,task three']
+        expected_tasks = ['0,todo,task one', '1,todo,task two', '2,todo,task three']
 
         input_tasks = ['task one', 'task two', 'task three']
         for task in input_tasks:
             add_task(task, self.file_name)
         actual_tasks = list_task(self.file_name)
         self.assertEqual(expected_tasks, actual_tasks)
-
-    def test_done_task(self):
-        pass
 
     def test_get_last_index(self):
         '''
@@ -56,4 +53,42 @@ class TaskTest(unittest.TestCase):
         actual_result = get_last_index(lines)
         self.assertEqual(expected_result, actual_result)
          
+    def test_make_it_done(self):
+        input_tasks = ['task one', 'task two', 'task three']
+        for task in input_tasks:
+            add_task(task, self.file_name)
+        make_it_done(1, self.file_name)
+        tasks = list_task(self.file_name)
+        self.assertEqual(tasks[0].split(',')[1], 'todo')
+        self.assertEqual(tasks[1].split(',')[1], 'done')
+
+    def test_save_task_write_mode(self):
+        # mode: write
+        expected_tasks = [
+            {'status': 'todo', 'name': 'task1'},
+            {'status': 'todo', 'name': 'task1'},
+        ]
+        expected_result = [
+            f"{i},{task['status']},{task['name']}\n"
+            for i, task
+            in enumerate(expected_tasks)
+        ]
+        save_task(expected_tasks, 'w', self.file_name)
+        with open(self.file_name, 'r') as f:
+            self.assertEqual(f.readlines(), expected_result)
         
+        # mode: append
+        additional_tasks = [
+            {'status': 'todo', 'name': 'task1'},
+            {'status': 'todo', 'name': 'task1'},
+        ]
+        additional_expected_result = [
+            f"{i},{task['status']},{task['name']}\n"
+            for i, task
+            in enumerate(expected_tasks + additional_tasks)
+        ]
+        save_task(additional_tasks, 'a', self.file_name)
+        with open(self.file_name, 'r') as f:
+            self.assertEqual(f.readlines(), additional_expected_result)
+
+    # def test_save_task_contains_comma(self):
